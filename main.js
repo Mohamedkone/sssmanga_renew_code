778456021a68bdf1d824b6ff7f0692ab7771d9cc
MohamedKone <mohamedkone11@outlook.fr>
2021-04-02 16:26:05 -0400

    finished the menu toggle option with js now working on the night shift

---
+++ main.js
@@ -0,0 +1,42 @@
+var menu = document.getElementById('burger');
+var log = document.getElementById('log');
+var nav = document.getElementById('top-nav');
+function dropDown(){
+
+    if (nav.style.display == "block"){
+        nav.style.display = "none";
+        log.style.display = "none";
+    }else{
+        nav.style.display = "block";
+        log.style.display = "block";
+    }
+}
+
+function navResize(){
+    if(screen.width > 768){
+
+        if (nav.style.display !== "flex" ){
+            nav.style.display = "flex";
+            log.style.display = "block";
+        }
+    }else{
+        if(nav.style.display == 'flex'){
+            nav.style.display = 'none';
+            log.style.display = 'none';
+        }
+
+    }
+}
+
+
+var l_switch = document.getElementById("mode");
+
+function light_switch(){
+    var l_switch = document.getElementById("mode");
+        if(l_switch.src == 'assets/dark_shift.svg'){
+            l_switch.src = 'assets/light_shift.svg';
+    }
+        else{
+            l_switch.src = 'assets/dark_shift.svg';
+    }
+}
